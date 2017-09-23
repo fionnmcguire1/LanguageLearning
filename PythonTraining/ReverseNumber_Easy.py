e50deb46fbd8e9098dbfbf95b7657897085bb144
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-09-23 11:25:28 +0100

    Removing duplicate files

--- PythonTraining/ReverseNumber_Easy.py
+++ PythonTraining/ReverseNumber_Easy.py
@@ -2,6 +2,7 @@
 Author: Fionn Mcguire
 Date: 21-09-2017
 Description: To Reverse a number inputted to the function and return a 0 if the 32 bit int overflows
+Added functionality to check if the nuber provided is a pallindrome
 '''
 
 
@@ -14,8 +15,13 @@ def reverse(x):
     while i > 0:
         z+=y[i-1]
         i-=1
+    if z == y:
+        print("{0} a pallindrome!".format(z))
     x = int(z)
     return(x)
 num = reverse(1453)
 print(num)
+num = reverse(1001)
+
+
 
