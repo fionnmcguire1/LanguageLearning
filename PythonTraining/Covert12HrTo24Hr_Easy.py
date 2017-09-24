3a3ded44906825a087c00280d7cf6fd1b5cc3ec8
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-09-24 20:47:27 +0100

    Completed all test cases

--- PythonTraining/Covert12HrTo24Hr_Easy.py
+++ PythonTraining/Covert12HrTo24Hr_Easy.py
@@ -4,22 +4,24 @@ Date: 21-09-2017
 Description: Covert 12 hour time format to 24 hour  
 '''
 
-
-
-
 import sys
 
 def timeConversion(s):
     # Complete this function
     if s[-2:] == "PM":
-        newHour = int(s[0:2])+12
-        s = str(newHour)+s[2:len(s)]
+        newHour = (int(s[0:2])+12)%24
+        if newHour != 0:
+            s = str(newHour)+s[2:len(s)]
         s = s[0:-2]
     else:
+        newHour = (int(s[0:2])+12)%24
+        if newHour == 0:
+            newHour = "00"
+            s = str(newHour)+s[2:len(s)]
         s = s[0:-2]
     return(s)
         
 
-s = "07:05:45PM"
+s = input().strip()
 result = timeConversion(s)
 print(result)
