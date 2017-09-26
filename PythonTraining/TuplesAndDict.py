c82951ff6437227ed57d7afc130b5bb2851727b1
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-09-26 19:51:38 +0100

    Playing with tuples

---
+++ PythonTraining/TuplesAndDict.py
@@ -0,0 +1,14 @@
+'''
+Author: Fionn Mcguire
+Date: 26-09-2017
+
+Description:
+    Working with tuples and dictionaries
+'''
+#These are useful for throwing together a load of different data types
+#Issue is you can't change the value of a tuple object
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5 )
+tup3 = "a", "b", "c", "d"
+tup4 = (5)
+print(tup4)
