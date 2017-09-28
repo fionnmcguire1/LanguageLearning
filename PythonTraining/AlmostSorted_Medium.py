6f66e363f7a4d64e51025cf39f779f8ef4c68c66
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-09-28 23:36:58 +0100

    Reduce Big O Notation

--- PythonTraining/AlmostSorted_Medium.py
+++ PythonTraining/AlmostSorted_Medium.py
@@ -7,16 +7,14 @@ reverse. If this finishes off sorting the list then print yes and how
 it was done. Otherwise print no
 
 '''
-
-
-
 n = int(input())
 listToBeSorted = list(map(int, input().strip().split(' ')))
 sortedList = sorted(listToBeSorted)
 i,j = 0,0
 checker = False
+operation = "swap"
 while i < n:
-    j = 0
+    j = i+1
     while j < n:
         listToBeSorted[i],listToBeSorted[j] = listToBeSorted[j],listToBeSorted[i]
         if sortedList == listToBeSorted:
@@ -27,26 +25,19 @@ while i < n:
             break 
         else:
             listToBeSorted[i],listToBeSorted[j] = listToBeSorted[j],listToBeSorted[i]
-        
-        j+=1
-    i+=1
-if checker == False:
-    i,j = 0,0
-    while i < n:
-        j = 0
-        while j < n:
             listToBeSorted[i:j] = listToBeSorted[i:j][::-1]
             if sortedList == listToBeSorted:
                 print("yes")
-                print("reverse {0} {1}".format(i+1,j+1))
+                print("reverse {0} {1}".format(i+1,j))
                 checker = True
-                i+=1
+                i=n+1
                 break
             else:
                 listToBeSorted[i:j] = listToBeSorted[i:j][::-1]
-
-            j+=1
-        i+=1
+        
+        
+        j+=1
+    i+=1
     
 if checker == False:
     print("no")
