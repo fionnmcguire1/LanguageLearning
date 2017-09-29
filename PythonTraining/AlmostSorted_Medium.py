c3acbc56cf4c5566aa82ddb7393bbe4f54a574e6
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-09-29 20:52:39 +0100

    Optimised with for loops

--- PythonTraining/AlmostSorted_Medium.py
+++ PythonTraining/AlmostSorted_Medium.py
@@ -10,34 +10,28 @@ it was done. Otherwise print no
 n = int(input())
 listToBeSorted = list(map(int, input().strip().split(' ')))
 sortedList = sorted(listToBeSorted)
-i,j = 0,0
-checker = False
-operation = "swap"
-while i < n:
+i,j,checker = 0,0,False
+for i in range(n):
     j = i+1
-    while j < n:
+    for j in range(j,n):
         listToBeSorted[i],listToBeSorted[j] = listToBeSorted[j],listToBeSorted[i]
         if sortedList == listToBeSorted:
             print("yes")
             print("swap {0} {1}".format(i+1,j+1))
-            checker = True
-            i = n+1
+            checker = True           
             break 
         else:
             listToBeSorted[i],listToBeSorted[j] = listToBeSorted[j],listToBeSorted[i]
-            listToBeSorted[i:j] = listToBeSorted[i:j][::-1]
-            if sortedList == listToBeSorted:
-                print("yes")
-                print("reverse {0} {1}".format(i+1,j))
-                checker = True
-                i=n+1
-                break
-            else:
+            if j != i+1 and j != i:
                 listToBeSorted[i:j] = listToBeSorted[i:j][::-1]
-        
-        
-        j+=1
-    i+=1
-    
+                if sortedList == listToBeSorted:
+                    print("yes")
+                    print("reverse {0} {1}".format(i+1,j))
+                    checker = True               
+                    break
+                else:
+                    listToBeSorted[i:j] = listToBeSorted[i:j][::-1]        
+    if checker == True:
+        break  
 if checker == False:
     print("no")
