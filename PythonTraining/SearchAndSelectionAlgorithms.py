1a421546eef6adf8e98843e36e9e27009f425cec
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-07 23:14:14 +0100

    Implemented quicksort

--- PythonTraining/SearchAndSelectionAlgorithms.py
+++ PythonTraining/SearchAndSelectionAlgorithms.py
@@ -23,16 +23,17 @@ def findmin (sample_dataset):
                 temp = sample_dataset[i]
         i+=1
     return temp
-
+'''
 min_num = findmin(sample_dataset)
 print(min_num)
 min_num = findmin(sample_dataset_ordered)
 print(min_num)
-print(max(sample_dataset))
+print(max(sample_dataset))'''
 
-
-sample_dataset_ordered = [0,1,4,7,9,44,66,77,87,98,100]
-
+#Use binary search to efficiently return the position of a value within
+#a sorted list. This is particularly efficient with large sorted lists. The algorithm defaults to
+# return a message Not found but can easily be altered to return an integer or null value as
+#to not mess with the rest of a program which may be utilizing this method
 def BinarySearch(arr,value):
     lowIndex = 1
     highIndex = len(arr)
@@ -45,9 +46,32 @@ def BinarySearch(arr,value):
         elif arr[middle] < value:
             lowIndex = middle
     return "Not found"
-num = BinarySearch(sample_dataset_ordered,112)
-print(num)
-    
+'''num = BinarySearch(sample_dataset_ordered,112)
+print(num)'''
+
+
+def partition(array, begin, end):
+    pivot = begin
+    for i in range(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
 
 
 
+def quicksort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    def _quicksort(array, begin, end):
+        if begin >= end:
+            return
+        pivot = partition(array, begin, end)
+        _quicksort(array, begin, pivot-1)
+        _quicksort(array, pivot+1, end)
+    return _quicksort(array, begin, end)
+
+print(sample_dataset)
+quicksort(sample_dataset)
+print(sample_dataset)
