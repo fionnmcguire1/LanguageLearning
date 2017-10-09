a6477a178ed518e0fd36609d572fc289cdfd9947
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-09 12:19:34 +0100

    Day 2 Arithmetic operations

--- PythonTraining/30DaysOfCoding.py
+++ PythonTraining/30DaysOfCoding.py
@@ -41,3 +41,24 @@ print(floatinput+d)
 # Concatenate and print the String variables on a new line
 str2 = s+str1
 print(str2)
+
+'''Day 2'''
+def get_total_cost_of_meal():
+    # original meal price
+    meal_cost = float(input())
+    # tip percentage
+    tip_percent = int(input())
+    # tax percentage
+    tax_percent = int(input())
+
+    # Write your calculation code here
+    tip = (meal_cost/100)*tip_percent# calculate tip
+    tax = (meal_cost/100)*tax_percent# caclulate tax
+
+    # cast the result of the rounding operation to an int and save it as total_cost
+    total_cost = int(round(meal_cost+tip+tax))
+
+    return str(total_cost)
+
+# Print your result
+print("The total meal cost is " + get_total_cost_of_meal() + " dollars.")
