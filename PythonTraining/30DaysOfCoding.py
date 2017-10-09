bd985250e71c4221491b87c3c155ee58ffff19e3
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-09 12:15:15 +0100

    Actual day 1 complete using data types

--- PythonTraining/30DaysOfCoding.py
+++ PythonTraining/30DaysOfCoding.py
@@ -13,7 +13,7 @@ print('Hello, World.')
 #Output string taken in
 print(input_string)
 
-'''Day 1'''
+'''Day 0.5 (Accidentally did one of the other tasks)'''
 #Print the sum of imputted array where the input is 2 lines the
 #First input is N (size of the array)
 #Second line is the array
@@ -24,3 +24,20 @@ result = 0
 for index,i in enumerate(array):
     result+=int(array[index])
 print(result)
+
+'''Day 1'''
+i = 15
+d = 4.0
+s = " there buddy"
+
+# Read and save an integer, double, and String to your variables.
+integerinput = int(input())
+floatinput = float(input())
+str1 = str(input())
+# Print the sum of both integer variables on a new line.
+print(integerinput+i)
+# Print the sum of the double variables on a new line.
+print(floatinput+d)
+# Concatenate and print the String variables on a new line
+str2 = s+str1
+print(str2)
