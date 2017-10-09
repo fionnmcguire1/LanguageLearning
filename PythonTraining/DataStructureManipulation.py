94b074b7e9b825a82a3d5ec7e879fca4a5c8ab45
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-09 17:31:23 +0100

    Left rotation built and tested

---
+++ PythonTraining/DataStructureManipulation.py
@@ -0,0 +1,32 @@
+'''
+Author: Fionn Mcguire
+Date: 09-10-2017
+Description: Working with and manipulating various data structures
+
+'''
+
+
+
+
+
+
+def leftRotation(a, d):
+    # Complete this function
+    if d != 0:
+        b = [0 for i in range(len(a))]
+        for i in range(len(a)):
+            c = (i-d)%(len(a))
+            b[c] = a[i]
+        str1 = ""
+    else:
+        b = a
+    for i in range(len(b)):
+        str1+=str(b[i])+" "
+    return(b)
+
+if __name__ == "__main__":
+    n, d = input().strip().split(' ')
+    n, d = [int(n), int(d)]
+    a = list(map(int, input().strip().split(' ')))
+    result = leftRotation(a, d)
+    print (" ".join(map(str, result)))
