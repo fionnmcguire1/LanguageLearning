3bce8db0829b24a8b90d820961a692af00bb4e87
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-13 23:03:17 +0100

    Ignoring true return fixed

--- PythonTraining/TreesAndGraphs.py
+++ PythonTraining/TreesAndGraphs.py
@@ -16,44 +16,41 @@ class Node(object):
                 self.childleft = Node(d)
             else:
                 self.childleft.insert(d)
-
         else:
             if self.childright == None:
                 self.childright = Node(d)
             else:
                 self.childright.insert(d)
     def contains(self,d):
-        if self.data == d:
+        #print("Looking at: {} for {}".format(self.data,d))
+        if d == self.data:
             return True
         else:
-            if d >= self.data:
-                if self.childleft == None:
-                    return False
-                else:
+            if d > self.data:
+                if self.childleft:
                     self.childleft.contains(d)
             else:
-                if self.childright == None:
-                    return False
-                else:
+                if self.childright:
                     self.childright.contains(d)
+            return False
+
 
 
 NewNode = Node(8)
-NewNode.insert(9)
-NewNode.insert(19)
-NewNode.insert(59)
-NewNode.insert(4)
-NewNode.insert(6)
 NewNode.insert(7)
-NewNode.insert(14)
+NewNode.insert(6)
+NewNode.insert(4)
+NewNode.insert(9)
+NewNode.insert(10)
 NewNode.insert(11)
-NewNode.insert(62)
-NewNode.insert(81)
-NewNode.insert(55)
-NewNode.insert(32)
+NewNode.insert(14)
 NewNode.insert(18)
 NewNode.insert(19)
-NewNode.insert(10)
+NewNode.insert(32)
+NewNode.insert(55)
+NewNode.insert(59)
+NewNode.insert(62)
+NewNode.insert(81)
 
 print(NewNode.contains(14))
 print(NewNode.contains(4))
@@ -62,3 +59,5 @@ print(NewNode.contains(8))
 print(NewNode.contains(18))
 print(NewNode.contains(0))
 print(NewNode.contains(1999999))
+
+#TTTTTFF
