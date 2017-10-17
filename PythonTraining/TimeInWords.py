432434b954b8e1a2f92668014d89543d3ce93bb5
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-17 21:53:32 +0100

    5 out of 9 test cases

--- PythonTraining/TimeInWords.py
+++ PythonTraining/TimeInWords.py
@@ -4,22 +4,23 @@ Date: 17-10-2017
 Description: Translate numerical time into textual
 '''
 
-h = int(input().strip())
-m = int(input().strip())
-hours = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve"]
-minutes = hours+["thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty one","twenty two", "twenty three","twenty four","twenty five","twenty six", "twenty seven","twenty eight","half"]
+def timeInWords(h,m):
+    hours = ["one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve"]
+    minutes = hours+["thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty","twenty one","twenty two", "twenty three","twenty four","twenty five","twenty six", "twenty seven","twenty eight","twenty nine","half"]
+    m-=1
+    h-=1
+    minstr = "minutes"
+    if m == 0:
+        minstr = "minute"
 
-h-=1
-minstr = "minutes"
-if m == 0:
-    minstr = "minute"
+    if m == -1:
+        message = hours[h]+" o' clock"
+    elif m == 29:
+        message = minutes[m]+" "+minstr+" past "+hours[h]
+    elif m > 29:
+        message = minutes[58-m]+" "+minstr+" to "+hours[h+1]
+    else:
+        message = minutes[m]+" "+minstr+" past "+hours[h]
+    print(message)
 
-if m == 0:
-    message = hours[h]+" o' clock"
-elif m == 30:
-    message = minutes[m]+" "+minstr+" past "+hours[h]
-elif m > 30:
-    message = minutes[59-m]+" "+minstr+" to "+hours[h+1]
-else:
-    message = minutes[m]+" "+minstr+" past "+hours[h]
-print(message)
+timeInWords(7,29)
