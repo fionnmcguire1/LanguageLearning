dc44943b22597091fc5ab91df52a526170cdedb6
fionnmcguire1 <fionnmcguire1@gmail.com>
2017-10-29 15:06:46 +0000

    Playing around with maps

--- PythonTraining/DataStructurePractice/TuplesAndDict.py
+++ PythonTraining/DataStructurePractice/TuplesAndDict.py
@@ -14,3 +14,30 @@ tup2 = (1, 2, 3, 4, 5 )
 tup3 = "a", "b", "c", "d"
 tup4 = (5)
 print(tup4)
+
+
+#Dictionaries
+mydict = {"Fionn": 23, "Richie": 24, "Sarah": 24, "Sarabeth": 28, "Katie": 33}
+
+print(mydict)
+#print element
+print(mydict["Fionn"])
+sortedKeys = sorted(mydict.keys())
+sortedValues = sorted(mydict.values())
+del mydict["Richie"]
+#Delete from dictionary
+print(mydict)
+
+#Check if key in dictionary
+if "James" in mydict:
+    print(True)
+elif "James" not in mydict:
+    print(False)
+#iterate through dictionary
+for i,j in mydict.items():
+    print("{} with value {}".format(i,j))
+
+#Another way to iterate through mydict
+for i in mydict:
+    print(i)
+    print(mydict[i])
