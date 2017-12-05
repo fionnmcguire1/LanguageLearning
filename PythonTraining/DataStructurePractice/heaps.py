a976555b1c28804da52d8c171c491d88ac86757a
Fionn Mcguire <fionnmcguire1@gmail.com>
2017-12-05 15:46:33 +0000

    Making heap more dynamix with min/max heap option

--- PythonTraining/DataStructurePractice/heaps.py
+++ PythonTraining/DataStructurePractice/heaps.py
@@ -6,35 +6,37 @@ Description: Practicing Heaps
 
 #Implementation of heap using array
 heap = []
-def addToHeap(heap,data):
+def addToHeap(heap,data,mode='max'):
     length = len(heap)
     i = 0
-    while i < length:
-        if heap[i] <= data:
-            heap.insert(i,data)
-            return heap
-        i+=1
-    heap.append(data)
-    return heap
+    if mode == 'max':
+        while i < length:
+            if heap[i] <= data:
+                heap.insert(i,data)
+                return heap
+            i+=1
+        heap.append(data)
+        return heap
+    else if mode == 'min':
+        #The only difference here is the sign
+        #Rather than having to run a check every time in 
+        #The above loop, it's better to check once at the call
+        #and reduce the total number of operations
+        #This does however lead to duplicate code
+        while i < length:
+            if heap[i] >= data:
+                heap.insert(i,data)
+                return heap
+            i+=1
+        heap.append(data)
+        return heap
 
 
-def addToMinHeap(heap,data):
-    heap.append(data)
-    i = len(heap)-1
-    while i > 0:
-        if heap[i] < heap[i-1]:
-            heap[i],heap[i-1] = heap[i-1],heap[i]
-            i-=1
-        else:
-            break
-    return heap
-
-
+#Populating heap with loop
 for i in range(20):
     heap = addToHeap(heap,i)
-    
 
-
+#Mixing it up
 heap = addToHeap(heap,5)
 heap = addToHeap(heap,6)
 heap = addToHeap(heap,7)
