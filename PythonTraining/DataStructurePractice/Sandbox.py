e193628a1d0e32fae7669f683c7c4ef80d4de754
Fionn Mcguire <fionnmcguire1@gmail.com>
2020-11-01 14:46:11 +0000

    Making graphs out of dictionaries

--- PythonTraining/DataStructurePractice/Sandbox.py
+++ PythonTraining/DataStructurePractice/Sandbox.py
@@ -1,4 +1,8 @@
-#Trees and Graphs
+##############################################
+####### ******************************** #####
+####### ******** DSA_BinaryTree ******** #####
+####### ******************************** #####
+##############################################
 
 class BinaryNode(object):
     def __init__(self,val):
@@ -69,18 +73,124 @@ class BinaryTree(object):
         print(current_node.val)
 
 
+##############################################
+## Testing Binary Tree Different Traversals ##
+##############################################
 
-new_tree = BinaryTree()
+# new_tree = BinaryTree()
+#
+# random_list = [4,7,56,34,2,52,12,1,1,6,7,865,0]
+#
+# for i in random_list:
+#     new_tree.addNode(i)
+#
+# print("In")
+# new_tree.inOrderPrint()
+# print("Pre")
+# new_tree.preOrderPrint()
+# print("Post")
+# new_tree.postOrderPrint()
 
-random_list = [4,7,56,34,2,52,12,1,1,6,7,865,0]
-# Soreted random list = [0,1,1,2,4,6,7,7,12,34,52,56,865] for reference on result
+##############################################
+####### ******************************** #####
+####### *********** DSA_Graph ********** #####
+####### ******************************** #####
+##############################################
 
-for i in random_list:
-    new_tree.addNode(i)
 
-print("In")
-new_tree.inOrderPrint()
-print("Pre")
-new_tree.preOrderPrint()
-print("Post")
-new_tree.postOrderPrint()
+#Dictionary based graph
+new_graph = {}
+
+def addNode(graph,val):
+    if val in graph.keys():
+        return
+    else:
+        new_graph[val] = []
+
+def addEdge(graph,node,path_node):
+    try:
+        if path_node in graph[node]:
+            return
+        else:
+            graph[node].append(path_node)
+    except:
+        pass
+
+def graphDFS(graph,source,dest,visited=None):
+    if visited is None:
+        visited = [source]
+    if source == dest:
+        print(visited)
+    else:
+        for node in graph[source]:
+            if node not in visited:
+                graphDFS(graph,node,dest,visited+[node])
+
+def graphBFS(graph,source,dest,visited=None):
+    if visited is None:
+        visited = [source]
+    if source == dest:
+        print(visited)
+    elif dest in graph[source]:
+        visited.append(dest)
+        print(visited)
+    else:
+        for node in graph[source]:
+            if node not in visited:
+                graphBFS(graph,node,dest,visited+[node])
+
+
+addNode(new_graph,"A")
+addNode(new_graph,"B")
+addNode(new_graph,"C")
+addNode(new_graph,"D")
+addNode(new_graph,"E")
+addNode(new_graph,"F")
+addNode(new_graph,"G")
+addNode(new_graph,"H")
+addNode(new_graph,"I")
+
+addEdge(new_graph,"A","B")
+addEdge(new_graph,"A","C")
+addEdge(new_graph,"A","D")
+addEdge(new_graph,"B","D")
+addEdge(new_graph,"B","I")
+addEdge(new_graph,"B","H")
+addEdge(new_graph,"H","G")
+
+print("Path")
+graphDFS(new_graph,"A","G")
+graphDFS(new_graph,"B","G")
+print("No Path")
+graphDFS(new_graph,"H","A")
+
+print("Path")
+graphBFS(new_graph,"A","G")
+graphBFS(new_graph,"B","G")
+print("No Path")
+graphBFS(new_graph,"H","A")
+
+
+#Object based Graph
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################
+####### ******************************** #####
+####### *********** DSA_Trie *********** #####
+####### ******************************** #####
+##############################################
